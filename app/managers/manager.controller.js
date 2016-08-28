a207aa34266ad3dd364a45a91faf3f04d23e1c90
maouissaoui <aouissaoui.moufida@gmail.com>
2016-08-28 21:31:59 +0200

    manager edit

--- app/managers/manager.controller.js
+++ app/managers/manager.controller.js
@@ -1,14 +1,19 @@
-(function () {
+
     'use strict';
 
     angular
         .module('app')
-        .controller('ManagerController', ManagerController);
-        ManagerController.$inject =['$scope','ManagerService','$routeParams','$location'];
-
-      function ManagerController($scope,ManagerService,$routeParams,$location){
+        .controller('ManagerController', function ManagerController($scope,ManagerService,$routeParams,$location){
     ManagerService.findById($routeParams.id).success(function(resp){
 	        $scope.manager = resp;
 	    });
-  };
-})();
+
+      $scope.delete=function(){
+            var r = confirm("Voulez vous supprimer ce manager ?");
+            if (r == true) {
+                ManagerService.deleteManager($scope.manager.id).then(function(){
+                    $location.path('/managers');
+                })
+            }
+        }
+      });
